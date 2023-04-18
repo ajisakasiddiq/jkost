e279a6df75ed850f189de38d899249794548f39d
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-04-18 20:58:59 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/', function () {
 
 
 Route::prefix('admin')
+    // ->middleware('auth', 'isAdmin')
     ->namespace('App\Http\Controllers\Admin')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard-admin');
