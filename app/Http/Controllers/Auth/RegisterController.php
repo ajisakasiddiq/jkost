d4fd306ae46125f601b43632b858192a7c36f36b
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-04 10:38:02 +0700

    uppp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,9 +51,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255'],
-            'level' => ['required', 'string', 'max:255'],
-            'slug' => ['required', 'string', 'max:255'],
+            // 'username' => ['required', 'string', 'max:255'],
+            // 'level' => ['required', 'string', 'max:255'],
+            // 'slug' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -69,9 +69,9 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'username' => $data['username'],
-            'level' => $data['level'],
-            'slug' => $data['slug'],
+            // 'username' => $data['username'],
+            // 'level' => $data['level'],
+            // 'slug' => $data['slug'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
