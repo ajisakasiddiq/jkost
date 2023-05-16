e0b475f3b327d34dc4425e3ce17d90d504d0188f
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-05-16 07:31:28 +0700

    update

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -1,62 +1,67 @@
 <?php
 
-namespace App\Http\Controllers\api;
+namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
-            if (password_verify($request->password, $user->password)) {
-                return response()->json([
-                    'success' => 1,
-                    'message' => 'selamat datang' . $user->name,
-                    'user' => $user
-                ]);
-            }
-            return $this->error('Password Salah');
-        }
-        return $this->error('Email Tidak Terdaftar');
-    }
 
     public function register(Request $request)
     {
-        $validasi = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
-            'email' => 'required|unique:users',
-            'password' => 'required|min:6',
+            'email' => 'required|email',
+            'password' => 'required',
+
         ]);
 
-        if ($validasi->fails()) {
-            $val = $validasi->errors()->all();
-            return $this->error($val);
-        }
-
-        $user = User::create(array_merge($request->all(), [
-            'password' => bcrypt($request->password)
-        ]));
-
-        if ($user) {
+        if ($validator->fails()) {
             return response()->json([
-                'success' => 1,
-                'message' => 'Register berhasil',
+                'success' => false,
+                'message' => 'Ada kesalahan',
+                'data' => $validator->errors()
             ]);
         }
 
-        return $this->error('Registrasi gagal');
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sukses register',
+            'data' => $success
+        ]);
     }
 
-    public function error($pesan)
+    public function login(Request $request)
     {
-        return response()->json([
-            'success' => 0,
-            'message' => $pesan
-        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $auth = Auth::user();
+            $success['token'] = User::findOrFail($auth->id)->createToken('auth_token')->plainTextToken;
+            $success['name'] = $auth->name;
+            $success['email'] = $auth->email;
+            $success['password'] = $auth->password;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login sukses',
+                'data' => $success
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Cek email dan password lagi',
+                'data' => null
+            ]);
+        }
     }
 }
