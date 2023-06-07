93462f5c4d4c733b2e09ad59f7a586f73cfe22bc
Yudistira <99013168+yudistira02@users.noreply.github.com>
2023-06-07 22:22:04 +0700

    done
    mantap

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -173,10 +173,10 @@ class PemesananController extends Controller
 
     public function callback(Request $request)
     {
-        $serverKey = config('midtrans.server_key');
+        $serverKey = 'SB-Mid-server-LVNbQZaBDFbkgVJ51O4rcHIA';
         $hashed = hash('sha512', $request->order_id . $request->status_code . $request->gross_amount . $serverKey);
         if ($hashed == $request->signature_key) {
-            if ($request->transaction_statuS == 'capture') {
+            if ($request->transaction_status == 'settlement') {
                 $order = Transaction::find($request->order_id);
                 $order->update(['status' => 'paid']);
             }
