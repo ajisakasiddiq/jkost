d33c627f48880e59f832575f1a46c5be5750fd3c
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2023-06-11 10:03:43 +0700

    update

--- database/migrations/2023_04_07_131625_create_transactions_table.php
+++ database/migrations/2023_04_07_131625_create_transactions_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('total_price');
             $table->date('tgl_sewa');
             // $table->enum('status', ['unpaid', 'paid', 'batal']);
-            $table->string('status');
+            $table->string('status')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
